3df1ad42e5c2f091f111c16eb3a5cad61ca6c11c
wswordform <25746440+chaosWsF@users.noreply.github.com>
2025-03-12 18:06:44 -0600

    Add a simple downloader

--- video_scrapper/README.md
+++ video_scrapper/README.md
@@ -23,8 +23,9 @@ The m3u8 playlist hides video streams inside PNGs: [detail](https://github.com/y
 ### Tools
 
 1. Tried formatting outputs of YoutubeDL in `deprecated/download_m3u8.py`
-2. Adding the tool `app.py` built by Flask as webUI that 
+2. Added `app.py` built by Flask as webUI that 
    + has a bar of the download progress and the percentage of downloaded part
    + has a log section to catch exception and other outputs, enumerating them (1,2,3,...)
    + has a input bar to accept aria2c arguments
    + makes a folder for downloaded files under ~/Downloads/Videos, which needs to check folder existence
+3. Added `simple_downloader.py` for batch downloading with provided list of links
---
+++ video_scrapper/simple_downloader.py
@@ -0,0 +1,57 @@
+import os
+import csv
+import logging
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import sanitize_filename
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('download.log'),    # Log to file
+        logging.StreamHandler()    # Log to console
+    ]
+)
+
+
+def download_video(url, name, aria2c_args):
+    """Download function to download video from URL"""
+    ydl_opts = {
+            'external_downloader': 'aria2c',
+            'external_downloader_args': aria2c_args.split(),
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        title = info.get('title', 'unknown_video')
+        ext = info.get('ext', 'mp4')
+        safe_title = sanitize_filename(title, restricted=True)
+        dir_path = os.path.join(os.path.expanduser('~'), 'Downloads', 'Videos', safe_title)
+        os.makedirs(dir_path, exist_ok=True)
+    
+    safe_name = sanitize_filename(name, restricted=True)
+    file_path = os.path.join(dir_path, f'{safe_name}.{ext}')
+    logging.info(f'Downloading to {file_path}')
+    ydl_opts['outtmpl'] = {'default': file_path}
+
+    logging.info(f"Starting to download {name}")
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        
+        logging.info("Download completed successfully.")
+    except Exception as e:
+        logging.error(f"Download failed with exception: {e}")
+
+
+def video_links_generator(file_path):
+    """Generator function to yield name and link from a tab-separated CSV file."""
+    with open(file_path, mode='r', newline='', encoding='utf-8') as video_links:
+        links_reader = csv.DictReader(video_links, delimiter='\t')
+        yield from ((row['Name'], row['Link']) for row in links_reader)
+
+if __name__ == '__main__':
+    aria2c_args = '-c -j 16 -x 1'
+
+    for name, url in video_links_generator('video.csv'):
+        download_video(url, aria2c_args)
