f7f09a3557132afd4bb4ffee21c8d8a21430c6c4
wswordform <25746440+chaosWsF@users.noreply.github.com>
2025-07-20 19:36:30 -0600

    add a file to merge photos

---
+++ image_scrapper/img2pdf.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+
+
+# List your .webp files
+image_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
+filenames = os.listdir(image_folder)
+
+# Convert and collect images
+for f in filenames:
+    subdir = os.path.join(image_folder, f)
+    if os.path.isdir(subdir):
+        image_files = sorted([os.path.join(subdir, file) for file in os.listdir(subdir) if file.endswith(('webp', 'WEBP'))])
+        if image_files:
+            images = [Image.open(image_f).convert('RGB') for image_f in image_files]
+            save_loc = os.path.join(image_folder, f + '.pdf')
+            images[0].save(save_loc, save_all=True, append_images=images[1:])
+            print(f"Converted {len(images)} images in {f} to PDF: {save_loc}")
+    else:
+        print(f"Skipping {f}, not a directory.")
