9c9c9fd812570222b7958adb954e13230b5d801f
wswordform <25746440+chaosWsF@users.noreply.github.com>
2025-07-23 15:29:08 -0600

    add some formats

--- image_scrapper/img2pdf.py
+++ image_scrapper/img2pdf.py
@@ -5,12 +5,13 @@ from PIL import Image
 # List your .webp files
 image_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
 filenames = os.listdir(image_folder)
+image_format = ('.webp', 'WEBP', '.jpg', 'JPG', '.jpeg', 'JPEG', '.png', 'PNG')
 
 # Convert and collect images
 for f in filenames:
     subdir = os.path.join(image_folder, f)
     if os.path.isdir(subdir):
-        image_files = sorted([os.path.join(subdir, file) for file in os.listdir(subdir) if file.endswith(('webp', 'WEBP'))])
+        image_files = sorted([os.path.join(subdir, file) for file in os.listdir(subdir) if file.endswith(image_format)])
         if image_files:
             images = [Image.open(image_f).convert('RGB') for image_f in image_files]
             save_loc = os.path.join(image_folder, f + '.pdf')
